a13289aaaa7870b53e099cde056c1a2cd46e81b2
ThanhVinh <124181827+vinhdv2k4@users.noreply.github.com>
2024-10-30 17:08:28 +0700

    Delete Lab1 directory

--- Lab1/HelloWorld2_2.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld2_2 {
-
-    public static void main(String[] args){
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
